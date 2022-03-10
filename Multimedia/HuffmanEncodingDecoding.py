49da59583d2db977a41b2345f7af5eb963534909
Anik Modak <anikmodak.rucse@gmail.com>
2022-03-10 10:17:08 +0600

    Added Huffman Encoding Decoding
    Added Huffman Encoding Decoding

---
+++ Multimedia/HuffmanEncodingDecoding.py
@@ -0,0 +1,78 @@
+msg = input("Enter a string: ");
+
+# Calculating frequency
+freq = {}
+for c in msg:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+print(freq);
+nodes = freq
+
+# Creating tree nodes
+class NodeTree(object):
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return (self.left, self.right)
+
+    def nodes(self):
+        return (self.left, self.right)
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+#implementing huffman coding
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+huffmanCode = huffman_code_tree(nodes[0][0])
+
+print('\n Char | Huffman code ')
+print('----------------------')
+
+CharPoint = {}
+for (char, frequency) in freq:
+    print(' %-4r |%12s' % (char, huffmanCode[char]))
+    CharPoint[huffmanCode[char]] = char
+print('----------------------\n')
+
+# enecrypt
+encrypt = ""
+for i in range(len(msg)):
+    encrypt += huffmanCode[msg[i]]
+    print(" %s => %s" % (msg[i], huffmanCode[msg[i]]))
+
+print("\nEncyption data : ", encrypt)
+
+# decrypt
+tmp = ""
+decode = ""
+for i in range(len(encrypt)):
+    tmp += encrypt[i]
+    for j in CharPoint:
+        if j == tmp:
+            decode += CharPoint[tmp]
+            tmp = ""
+
+print("\nDecryption data : ", decode)
